b52e12e7f25da8c1ca1c945099d4c71ccb63fbf3
Duc Bui <32723856+buianhduc@users.noreply.github.com>
2020-03-26 02:23:21 +0700

    update

--- search_bar.js
+++ search_bar.js
@@ -1,17 +1,31 @@
-fetch("https://raw.githubusercontent.com/buianhduc/Norona/master/database.json")
-  .then(response => response.json())
-  .then(json => console.log(json));
-console.log("jdjsndijw");
-// let options = {
-//     shouldSort: true,
-//     includeScore: true,
-//     threshold: 0.6,
-//     location: 0,
-//     distance: 100,
-//     minMatchCharLength: 1,
-//     keys: [
-//       "name", "category";
-//     ]
-//   };
-//   let fuse = new Fuse(list, options); // "list" is the item array
-//   let result = fuse.search("");
+var list;
+fetch("./database.json").then(function(response){
+    return response.json();
+}).then(function (obj){
+    list=obj;
+}).catch(function(error){
+    console.log("there is an error");
+})
+document.getElementById('input_box').onkeypress = function(e){
+    if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13'){
+
+        let options = {
+            shouldSort: true,
+            includeScore: true,
+            threshold: 0.6,
+            location: 0,
+            distance: 100,
+            minMatchCharLength: 1,
+            keys: [
+              "name","category"
+            ]
+          };
+          let fuse = new Fuse(list, options); // "list" is the item array
+        document
+          let result = fuse.search("");
+          console.log(result);
+      return false;
+    }
+  }
