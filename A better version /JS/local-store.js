551875d77715adc82e7736f9c26fa7f41d80ff3a
Duc Bui <32723856+buianhduc@users.noreply.github.com>
2020-03-31 21:07:11 +0700

    update CSS

--- A better version /JS/local-store.js
+++ A better version /JS/local-store.js
@@ -105,7 +105,7 @@ class UI {
       tempTotal += item.price * item.amount;
       itemsTotal += item.amount;
     });
-    cartTotal.innerText ='Tổng số tiền:  ' + parseFloat(tempTotal.toFixed(2));
+    cartTotal.innerText ='Tổng số tiền:  ' + parseFloat(tempTotal.toFixed(2))+' VND';
     // cartItems.innerText= itemsTotal;
     // console.log(cartTotal, itemsTotal); //ddcmm lam lai cho nay di
   }
